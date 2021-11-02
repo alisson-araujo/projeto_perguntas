3b84761393770650f549bd68188debed60c7cddc
Alisson Araujo <alisson.ads@hotmail.com>
2021-11-01 22:55:40 -0300

    testando funcionalidades.

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,64 @@
 import 'package:flutter/material.dart';
 
-main() {
-  runApp(PerguntaApp());
-}
+main() => runApp(PerguntaApp());
+
 
 class PerguntaApp extends StatelessWidget {
 
+  @override
   Widget build(BuildContext context) {
+    final perguntas = [
+      'WTF IS GOING ON?',
+      'Hey Bro, are you OK?',
+    ];
+
     return MaterialApp(
-      home: Text('Olá Flutter!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Perguntas'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text(perguntas[0]),
+            TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.blue,
+              ),
+              child: Text(
+                'I do not know!!!',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: (){},
+            ),
+            TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.blue,
+              ),
+              child: Text(
+                'WHATT?!',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: null,
+            ),
+            TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.blue,
+              ),
+              child: Text(
+                'Are you serious?!',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: (){},
+            ),
+          ],
+        ),
+      ), 
     );
   }
 }
